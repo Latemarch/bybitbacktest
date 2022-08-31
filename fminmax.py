9b2b3523e23c936b5ade5f3318849fcce143b202
Latemarch <dolphin0331@naver.com>
2022-09-01 01:23:45 +0900

    +MACD

--- fminmax.py
+++ fminmax.py
@@ -1,3 +1,4 @@
+from turtle import showturtle
 import numpy as np
 import pandas as pd
 import gzip
@@ -7,6 +8,7 @@ import pandas as pd
 import time
 import datetime
 import plotly.graph_objects as go
+import plotly.subplots as ms
 
 from pandas.core.frame import DataFrame
 #mac
@@ -15,6 +17,7 @@ from pandas.core.frame import DataFrame
 
 def trading(position, buyorsell):
     i = 1 if position == 'long' else 0
+    stoploss[i] = k
     if buyorsell == 'buy':
         balance[i][0] = 1
         balance[i][1] = price
@@ -24,15 +27,17 @@ def trading(position, buyorsell):
         balance[i][2] += add
         if i == 1:
             if add > 0:tradingcountL[0] += 1
-            else: tradingcountL[1] +=1
+            else: 
+                tradingcountL[1] +=1
         else:
             if add > 0:tradingcountS[0] += 1
-            else: tradingcountS[1] +=1
+            else: 
+                tradingcountS[1] +=1
 def record_history(position,bors):
     i = 1 if position == 'long' else 0
-    k = 0 if bors == 'buy' else 2
-    history[i+k].append([candletime[-1]])
-    history[i+k][-1].append(price)
+    j = 0 if bors == 'buy' else 2
+    history[i+j].append([candletime[-1]])
+    history[i+j][-1].append(price)
 def local_extremum(a): 
     c = a*2-1
     b = a-1
@@ -52,22 +57,24 @@ def local_extremum(a):
 balance = [[0,0,100],[0,0,100]]
 history = [[[0,0]],[[0,0]],[[0,0]],[[0,0]]]
 localextrema = [[[0,0]],[[0,0]],[0],[0]]
-
+volume = 0
+global k
 ohlc_list = []
-ohlc = np.empty((1,4))
+ohlc = np.empty((1,5))
 candletime = []
 lenofcandle = []
+macd_osc = []
+macd = np.empty((0))
 k = 0
 trend = 0
 losstimeL = 0
 losstimeS = 0
-stopbuyings = 1
-stopbuyingl = 1
+stoploss =[0,0]
 tradingcountS= [0,0]
 tradingcountL= [0,0]
 
 start = 0
-last = 50
+last = 90
 for h in range(start,last):
 
     with gzip.open('/Users/jun/btcusd/%03d.gz' % h, 'rb') as f:
@@ -85,13 +92,17 @@ for h in range(start,last):
         tictime = float(daytics[1][0])
         minute = tictime//60
         candletime.append(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(float(daytics[1][0]))))
-        ohlc = np.append(ohlc,[[price,price,price,price]],axis =0)
+        ohlc = np.append(ohlc,[[price,price,price,price,price]],axis =0)
         ohlc = np.delete(ohlc,0, axis = 0)
 
     qty = 100/price
+    pp = price*0.02
+    ppl = pp/2
+    pp = price*0.01
+    ppmacd = pp/6
     for i, row in enumerate(daytics):
         ohlc_list.append(float(row[4]))
-        #tprice = float(row[4])
+        volume += (float(row[3]))
 
         # To make bundle of ticdatas having "same" time
         if tictime == float(row[0]): 
@@ -106,119 +117,133 @@ for h in range(start,last):
             k+=1
             minute = tictime//60
             candletime.append(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(float(tictime))))
-            ohlc = np.append(ohlc,[[ohlc_list[0],max(ohlc_list),min(ohlc_list),ohlc_list[-1]]],axis = 0)
+            ohlc = np.append(ohlc,[[ohlc_list[0],max(ohlc_list),min(ohlc_list),ohlc_list[-1],volume]],axis = 0)
             lenofcandle.append(max(ohlc_list)-min(ohlc_list))
+            volume = 0
             ohlc_list = []
         # 1 min candle ==================================================
             local_extremum(10)
 
         ###### Here, U can write ur strategy. U have bundle of ticdata(got once) from bybit server 
-            if not k > 120: continue
-            movingaverage1 = np.mean(ohlc[-20:,3])
-            movingaverage2 = np.mean(ohlc[-50:,3])
-            movingaverage3 = np.mean(ohlc[-120:,3])
-            if movingaverage1 > movingaverage2 > movingaverage3:
-                if not trend == 'long' and 20<k-losstimeS:stopbuyings = 0
-                trend = 'long'
-            elif movingaverage1 < movingaverage2 < movingaverage3: 
-                if not trend == 'short' and 20<k-losstimeL:stopbuyingl = 0
-                trend = 'short'
-            else: trend = 'None'
-        if not k > 120: continue
+            ma1 = np.mean(ohlc[-12:,3])
+            ma2 = np.mean(ohlc[-26:,3])
+            ma3 = np.mean(ohlc[-50:,3])
+            macd = np.append(macd,ma1-ma2)
+            macd_sig = np.mean(macd[-9:])
+            macd_osc.append(macd[-1] - macd_sig)
+
+        if not k > 50: continue
+
 
         price = float(row[4])
-        if balance[1][0]:
-            if price < lossprice_L:
+        if balance[1][0]:#and stoploss[1]+20 < k :
+            stoploss[1] = 0
+            if price < lossprice_L or price > profitprice_L:
                 trading('long','sell')
                 record_history('long','sell')
-                if price < lossprice_L : 
-                    losstimeL = k
-            elif price > profitprice_L:
+            elif macd_osc[-1] > 400:
                 trading('long','sell')
                 record_history('long','sell')
 
 
-        if balance[0][0]:
-            if lossprice_s < price or price < profitprice_s:
+        if balance[0][0]:#
+            stoploss[0] = 0
+            if lossprice_s < price or price < profitprice_s: 
                 trading('short','sell')
                 record_history('short','sell')
-                if lossprice_s < price: 
-                    stopbuyings = 1
-                    losstimeS = k
+            elif macd_osc[-1] < -10000:
+                trading('short','sell')
+                record_history('short','sell')
 
         #if balance[1][0] or balance[0][0]: continue
-        if trend == 'long':
-            if price < movingaverage1 and not balance[1][0] and not stopbuyingl:
-                trading('long','buy')
-                record_history('long','buy')
-                profitprice_L = price*1.02
-                lossprice_L = price*0.995
-                Lreached = 0
+        if not balance[1][0] and macd_osc[-1] < -ppmacd and stoploss[1]+20<k:
+            trading('long','buy')
+            record_history('long','buy')
+            profitprice_L = price+pp
+            lossprice_L = price-ppl
+            Lreached = 0
 
-        elif trend == 'short':
-            if not balance[0][0] and not stopbuyings and price > movingaverage1:
-                trading('short','buy')
-                record_history('short','buy')
-                profitprice_s = price*0.995
-                lossprice_s = price*1.02
-    p1 = int(100*tradingcountL[0]/(tradingcountL[0]+tradingcountL[1]))    
-    p2 = int(100*tradingcountS[0]/(tradingcountS[0]+tradingcountS[1]))    
-    print(h,'))',round(balance[1][2],2),'(',p1,')',round(balance[0][2],2),'(',p2,')',round(100*(float(daytics[-1][4])-float(daytics[0][4]))/float(daytics[0][4]),2))
-    #tradingcountS= [0,0]
-    #tradingcountL= [0,0]
+        if not balance[0][0] and macd_osc[-1] > ppmacd and stoploss[0]+20<k:
+            trading('short','buy')
+            record_history('short','buy')
+            profitprice_s = price-pp
+            lossprice_s = price+ppl
 
-'''
-#=============== Candle Chart =================
-index = pd.DatetimeIndex(candletime)
-ohlc_df = DataFrame(data=ohlc, index=index, columns=['open','high','low','close'])
-ohlc_df = ohlc_df.astype(float)
-ohlc_df['ma10'] = ohlc_df['close'].rolling(20).mean()
-ohlc_df['ma20'] = ohlc_df['close'].rolling(50).mean()
-ohlc_df['ma50'] = ohlc_df['close'].rolling(120).mean()
-ohlc_df['max'] = np.nan
-ohlc_df['min'] = np.nan
-ohlc_df['buy_long'] = np.nan
-ohlc_df['sell_long'] = np.nan
 
-for i in range(4): localextrema[i].pop(0)
-for i, val in enumerate(localextrema[0]):
-    ohlc_df.loc[val[0],'max'] = val[1] + 20
-for i, val in enumerate(localextrema[1]):
-    ohlc_df.loc[val[0],'min'] = val[1] - 20
+    p1 = tradingcountL[0]+tradingcountL[1]
+    p2 = tradingcountS[0]+tradingcountS[1]    
+    print(h,'))',round(balance[1][2],2),'(',tradingcountL[0],p1,')',round(balance[0][2],2),'(',tradingcountS[0],p2,')',round(100*(float(daytics[-1][4])-float(daytics[0][4]))/float(daytics[0][4]),2))
+    tradingcountS= [0,0]
+    tradingcountL= [0,0]
 
-k = 1 #select position u wanna see (0=short,1=long)
-for i in range(4): history[i].pop(0)
-for i, val in enumerate(history[k]):
-    ohlc_df.loc[val[0],'buy_long'] = val[1] - 200
-for i, val in enumerate(history[k+2]):
-    ohlc_df.loc[val[0],'sell_long'] = val[1] + 200
+if 1==2:
+    macd_osc.append(macd[-1] - macd_sig)
+    #=============== Candle Chart =================
+    index = pd.DatetimeIndex(candletime)
+    df = DataFrame(data=ohlc, index=index, columns=['open','high','low','close','volume'])
+    df = df.astype(float)
+    df['ma10'] = df['close'].rolling(12).mean()
+    df['ma20'] = df['close'].rolling(26).mean()
+    df['ma50'] = df['close'].rolling(120).mean()
+    df['MACD'] = df['ma10'] - df['ma20']    # MACD
+    df['MACD_Signal'] = df['MACD'].rolling(window=9).mean() # MACD Signal
+    df['MACD_Oscil'] = df['MACD'] - df['MACD_Signal']   #MACD 오실레이터
+    df['macdcd'] = macd_osc
+    df['max'] = np.nan
+    df['min'] = np.nan
+    df['buy_long'] = np.nan
+    df['sell_long'] = np.nan
 
-ma10 = go.Scatter(x=ohlc_df.index, y=ohlc_df['ma10'], line=dict(color='black', width=0.8), name='ma20')
-ma20 = go.Scatter(x=ohlc_df.index, y=ohlc_df['ma20'], line=dict(color='green', width=0.8), name='ma50')
-ma50 = go.Scatter(x=ohlc_df.index, y=ohlc_df['ma50'], line=dict(color='red', width=0.8), name='ma120')
-maxval = go.Scatter(x=ohlc_df.index, y=ohlc_df['max']+50, mode ="markers", marker=dict(color='green',symbol= '6'), name='Max')
-minval = go.Scatter(x=ohlc_df.index, y=ohlc_df['min']-50, mode ="markers", marker=dict(color='red',symbol = '5'), name='Min')
-history_SL = go.Scatter(x=ohlc_df.index, y=ohlc_df['sell_long'], mode ="markers", marker=dict(color='green',symbol= '6'), name='Sell long')
-history_BL = go.Scatter(x=ohlc_df.index, y=ohlc_df['buy_long'], mode ="markers", marker=dict(color='red',symbol = '5'), name='Buy long')
-candle = go.Candlestick(x=ohlc_df.index,open=ohlc_df['open'],high=ohlc_df['high'],low=ohlc_df['low'],close=ohlc_df['close'])
+    for i in range(4): localextrema[i].pop(0)
+    for i, val in enumerate(localextrema[0]):
+        df.loc[val[0],'max'] = val[1] + 20
+    for i, val in enumerate(localextrema[1]):
+        df.loc[val[0],'min'] = val[1] - 20
 
-#fig = go.Figure(data=[candle,history_SL,history_BL,ma10,ma20,ma50])
-fig = go.Figure(data=[candle,ma20,ma50,maxval,minval])
-#fig.write_image("fig1.svg")
-fig.show()
+    k = 1 #select position u wanna see (0=short,1=long)
+    for i in range(4): history[i].pop(0)
+    for i, val in enumerate(history[k]):
+        df.loc[val[0],'buy_long'] = val[1] - 200
+    for i, val in enumerate(history[k+2]):
+        df.loc[val[0],'sell_long'] = val[1] + 200
 
-mintomax = np.array(mintomax)
-mintomax = DataFrame(data=mintomax, columns =['mintomax'])
-maxtomin = np.array(maxtomin)
-maxtomin = DataFrame(data=maxtomin, columns =['maxtomin'])
-minmax = pd.concat([mintomax,maxtomin],axis=1)
-minmax.to_excel('minmax1.xlsx')
-ohlc_df.to_excel('ohlc.xlsx')
+    ma10 = go.Scatter(x=df.index, y=df['ma10'], line=dict(color='black', width=0.8), name='ma20')
+    ma20 = go.Scatter(x=df.index, y=df['ma20'], line=dict(color='green', width=0.8), name='ma50')
+    ma50 = go.Scatter(x=df.index, y=df['ma50'], line=dict(color='red', width=0.8), name='ma120')
+    maxval = go.Scatter(x=df.index, y=df['max']+50, mode ="markers", marker=dict(color='green',symbol= '6'), name='Max')
+    minval = go.Scatter(x=df.index, y=df['min']-50, mode ="markers", marker=dict(color='red',symbol = '5'), name='Min')
+    history_SL = go.Scatter(x=df.index, y=df['sell_long'], mode ="markers", marker=dict(color='green',symbol= '6'), name='Sell long')
+    history_BL = go.Scatter(x=df.index, y=df['buy_long'], mode ="markers", marker=dict(color='red',symbol = '5'), name='Buy long')
+    candle = go.Candlestick(x=df.index,open=df['open'],high=df['high'],low=df['low'],close=df['close'])
+    volume_bar = go.Bar(x=df.index,y=df['volume'], name='volume')
+    MACD = go.Scatter(x=df.index, y=df['MACD'], line=dict(color='blue', width=2), name='MACD', legendgroup='group2', legendgrouptitle_text='MACD')
+    MACD_Signal = go.Scatter(x=df.index, y=df['MACD_Signal'], line=dict(dash='dashdot', color='green', width=2), name='MACD_Signal')
+    MACD_Oscil = go.Bar(x=df.index, y=df['MACD_Oscil'], marker_color='red', name='MACD_Oscil')
+    #MACD_Oscil = go.Bar(x=df.index, y=df['macdcd'], marker_color='black', name='MACD_Oscil')
 
-#print(overshooting_short)
-#print(overshooting_long)
-'''
+    #fig = go.Figure(data=[candle,history_SL,history_BL,ma10,ma20,ma50])
+    #fig = go.Figure(data=[candle,ma20,ma50,maxval,minval])
+    fig = ms.make_subplots(rows=2, cols=1, shared_xaxes= True,shared_yaxes=False, vertical_spacing=0.02)
+    fig.add_trace(candle, row=1,col=1)
+    #fig.add_trace(MACD,row=2,col=1)
+    #fig.add_trace(MACD_Signal,row=2,col=1)
+    fig.add_trace(MACD_Oscil,row=2,col=1)
+    #fig.add_trace(volume_bar, row=2,col=1)
+    fig.update_layout(title='BTCUSD', xaxis1_rangeslider_visible = False)
+    #fig.write_image("fig1.svg")
+    fig.show()
+    '''
+    mintomax = np.array(mintomax)
+    mintomax = DataFrame(data=mintomax, columns =['mintomax'])
+    maxtomin = np.array(maxtomin)
+    maxtomin = DataFrame(data=maxtomin, columns =['maxtomin'])
+    minmax = pd.concat([mintomax,maxtomin],axis=1)
+    minmax.to_excel('minmax1.xlsx')
+    df.to_excel('ohlc.xlsx')
 
+    #print(overshooting_short)
+    #print(overshooting_long)
+    '''
 
 
 
